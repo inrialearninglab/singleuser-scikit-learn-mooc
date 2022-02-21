3a537bd15bf567741f7785d8b29a1399366b22e0
Benoit Rospars <benoit.rospars@inria.fr>
2022-02-21 15:10:09 +0100

    Update script to not use gist but create filtered notebooks directly

--- init_notebooks.py
+++ init_notebooks.py
@@ -6,17 +6,40 @@ import urllib.error
 import urllib.request
 import concurrent.futures
 
+repo_base_url = 'https://raw.githubusercontent.com/inrialearninglab/singleuser-scikit-learn-mooc/master/'
+skl_mooc_base_url = 'https://raw.githubusercontent.com/INRIA/scikit-learn-mooc/main/'
+skl_api_base_url = 'https://api.github.com/repos/INRIA/scikit-learn-mooc/contents/'
+
+notebook_dir = os.environ.get('NOTEBOOK_DIR')
+
+
+def get_filelist(directory):
+    filelist = []
+    print('Fetching : ' + directory)
+    with urllib.request.urlopen(skl_api_base_url + directory) as response:
+        entries = json.loads(response.read().decode())
+        for entry in entries:
+            if entry['type'] == 'file':
+                if '_sol_' not in entry['name']:
+                    filelist.append((os.path.join(notebook_dir, entry['path']), entry['download_url']))
+            elif entry['type'] == 'dir':
+                filelist.extend(get_filelist(entry['path']))
+            else:
+                print('Error entry type not supported')
+        return filelist
+
 
 def download_file(entry):
     path, url = entry
     if not (os.path.exists(path)):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
         try:
             urllib.request.urlretrieve(url, path)
             filename, ext = os.path.splitext(path)
             print('Downloaded ' + filename)
             if ext == ".ipynb":
                 add_nb_metadata(path, url)
-                print('Add metadata ' + filename)
+                print('Add metadata ' + path)
 
         # In case of request error
         except urllib.error.URLError:
@@ -33,68 +56,44 @@ def add_nb_metadata(notebook_path, notebook_url):
 
 
 def main():
-    # Url of the json files that lists all the needed content
-    base_url = 'https://gist.githubusercontent.com/brospars/c86986af126c17d3ede848b11e3a5a65/raw/'
-    notebook_list_url = base_url + 'notebooks.json'
-
-    notebook_dir = os.environ.get('NOTEBOOK_DIR')
     if not notebook_dir:
         print("Error notebook dir")
     elif not (os.path.isdir(notebook_dir)):
         print("Notebook dir not exists")
     else:
-        # Read json and iterate on notebooks and datasets
-        with urllib.request.urlopen(notebook_list_url) as content:
-            data = json.loads(content.read().decode())
+        download_entries = []
 
-            # Create /notebooks
-            if not os.path.exists(os.path.join(notebook_dir, 'notebooks')):
-                os.mkdir(os.path.join(notebook_dir, 'notebooks'))
+        # Add start guide notebook
+        nb_guide_url = repo_base_url + 'JupyterNotebookGuide.ipynb'
+        nb_guide_path = os.path.join(notebook_dir, 'notebooks', 'JupyterNotebookGuide.ipynb')
+        download_entries.append((nb_guide_path, nb_guide_url))
 
-            # Create /notebooks/helpers
-            if not os.path.exists(os.path.join(notebook_dir, 'notebooks', 'helpers')):
-                os.mkdir(os.path.join(notebook_dir, 'notebooks', 'helpers'))
+        dir_to_sync = ['datasets', 'figures', 'notebooks']
 
-            # Create /datasets
-            if not os.path.exists(os.path.join(notebook_dir, 'datasets')):
-                os.mkdir(os.path.join(notebook_dir, 'datasets'))
+        for directory in dir_to_sync:
+            download_entries.extend(get_filelist(directory))
 
-            # Create /figures
-            if not os.path.exists(os.path.join(notebook_dir, 'figures')):
-                os.mkdir(os.path.join(notebook_dir, 'figures'))
+        # Parallel download file using threads
+        with concurrent.futures.ThreadPoolExecutor() as exector:
+            exector.map(download_file, download_entries)
 
-            # Create list of file to download
-            download_entries = []
-            # Add start guide notebook
-            nb_guide_url = base_url + 'JupyterNotebookGuide.ipynb'
-            nb_guide_path = os.path.join(notebook_dir, 'notebooks', 'JupyterNotebookGuide.ipynb')
-            download_entries.append((nb_guide_path, nb_guide_url))
-
-            # Add notebooks
-            for notebook in data['notebooks']:
-                nb_path = os.path.join(notebook_dir, os.path.dirname(notebook), os.path.basename(notebook))
-                download_entries.append((nb_path, data['baseUrl'] + notebook))
-            # Add datasets
-            for dataset in data['datasets']:
-                data_path = os.path.join(notebook_dir, 'datasets', os.path.basename(dataset))
-                download_entries.append((data_path, data['baseUrl'] + dataset))
-            # Add figures
-            for figure in data['figures']:
-                figure_path = os.path.join(notebook_dir, 'figures', os.path.basename(figure))
-                download_entries.append((figure_path, data['baseUrl'] + figure))
-
-            # Parallel download file using threads
-            with concurrent.futures.ThreadPoolExecutor() as exector:
-                exector.map(download_file, download_entries)
-
-            # Create empty wrapups
-            for wrapup in data['wrapups']:
-                wrapup_path = os.path.join(notebook_dir, 'notebooks', os.path.basename(wrapup))
-                if not (os.path.exists(wrapup_path)):
-                    # Create empty notebook
-                    with open(wrapup_path, "w") as wrapup_nb:
-                        wrapup_nb.write('{"cells":[],"metadata":{},"nbformat":4,"nbformat_minor":4}')
-                        print('Creating ' + wrapup_path)
+        # Create empty wrapups
+        for wrapup in [
+            "evaluation/ensemble_questions.ipynb",
+            "evaluation/linear_models_questions.ipynb",
+            "evaluation/trees_questions.ipynb",
+            "evaluation/predictive_modeling_pipeline_questions.ipynb",
+            "evaluation/overfit_questions.ipynb",
+            "evaluation/tuning_questions.ipynb",
+            "evaluation/evaluation_questions.ipynb",
+            "evaluation/sandbox_notebook.ipynb"
+        ]:
+            wrapup_path = os.path.join(notebook_dir, 'notebooks', os.path.basename(wrapup))
+            if not (os.path.exists(wrapup_path)):
+                # Create empty notebook
+                with open(wrapup_path, "w") as wrapup_nb:
+                    wrapup_nb.write('{"cells":[],"metadata":{},"nbformat":4,"nbformat_minor":4}')
+                    print('Creating ' + wrapup_path)
 
 
 if __name__ == "__main__":
